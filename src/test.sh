25ebc975d44b35cc217de2009ae37defca395798
abdom <abdo@email.com>
2025-07-24 14:23:59 +0300

    test1

---
+++ src/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+EXPECTED="Hello, World!"
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
+if [ "$EXPECTED" = "$OUTPUT" ]; then
+    echo "app.js ran successfully."
+    exit 0
+else
+    echo "app.js failed to run."
+    exit 1
+fi
